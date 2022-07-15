bd34dde16e933aebd91832e93ac46c292e52f2a7
Konstantin <shuvalov.2021@list.ru>
2022-07-15 11:28:19 +0300

    feat: implemented users entity

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,39 +1,52 @@
 import {
+  Body,
+  ClassSerializerInterceptor,
   Controller,
+  Delete,
   Get,
   Param,
+  ParseUUIDPipe,
   Post,
-  Body,
   Put,
-  Delete,
+  UseInterceptors,
 } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { UserEntity } from './entity/user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
+@UseInterceptors(ClassSerializerInterceptor)
 @Controller('user')
 export class UsersController {
+  constructor(private readonly usersServices: UsersService) {}
+
   @Get()
-  getUsers() {
-    return 'return all users';
+  getUsers(): Array<UserEntity> {
+    return this.usersServices.getAll();
   }
+
   @Get(':id')
-  getUser(@Param('id') id: string) {
-    return `return user : ${id}`;
+  getUser(
+    @Param('id', new ParseUUIDPipe({ version: '4' })) id: string,
+  ): UserEntity {
+    return this.usersServices.getOne(id);
   }
+
   @Post()
-  createUser(@Body() createUserDto: CreateUserDto): string {
-    return `Login: ${createUserDto.login}, Password: ${createUserDto.password}`;
+  createUser(@Body() createUserDto: CreateUserDto): UserEntity {
+    return this.usersServices.create(createUserDto);
   }
+
   @Put(':id')
   updateUser(
     @Body() updateUserDto: UpdateUserDto,
-    @Param('id') id: string,
-  ): string {
-    return `id : ${id} New Password: ${updateUserDto.newPassword}, Old Password: ${updateUserDto.oldPassword}`;
+    @Param('id', new ParseUUIDPipe({ version: '4' })) id: string,
+  ): UserEntity {
+    return this.usersServices.update(id, updateUserDto);
   }
+
   @Delete(':id')
-  deleteUser(@Param('id') id: string): string {
-    console.log(id);
-    return `Delete user: ${id}`;
+  deleteUser(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string) {
+    return this.usersServices.delete(id);
   }
 }
