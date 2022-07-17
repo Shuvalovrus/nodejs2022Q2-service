8b5b41add08fc08e8a08b1d223deeb6e71cfe7b5
Konstantin <shuvalov.2021@list.ru>
2022-07-17 14:23:08 +0300

    fix: change track imports/exports

--- src/tracks/tracks.service.ts
+++ src/tracks/tracks.service.ts
@@ -1,19 +1,30 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  forwardRef,
+  Inject,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateTrackDto } from './dto/create-track.dto';
 import { v4 as uuidv4 } from 'uuid';
 import { TrackEntity } from './entity/track.entity';
 import { UpdateTrackDto } from './dto/update-track.dto';
+import { FavouritesService } from '../favourites/favourites.service';
 
 @Injectable()
 export class TracksService {
-  public static tracks: TrackEntity[] = [];
+  constructor(
+    @Inject(forwardRef(() => FavouritesService))
+    private favouritesService: FavouritesService,
+  ) {}
+
+  public tracks: TrackEntity[] = [];
 
   getAll(): Array<TrackEntity> {
-    return TracksService.tracks;
+    return this.tracks;
   }
 
   getOne(id: string): TrackEntity {
-    const track = TracksService.tracks.find((track) => track.id === id);
+    const track = this.tracks.find((track) => track.id === id);
     if (!track) throw new NotFoundException();
     return track;
   }
@@ -21,13 +32,13 @@ export class TracksService {
   create(createTrackDto: CreateTrackDto): TrackEntity {
     const newTrack = new TrackEntity({ id: uuidv4(), ...createTrackDto });
 
-    TracksService.tracks.push(newTrack);
+    this.tracks.push(newTrack);
 
     return newTrack;
   }
 
   update(id, updateTrackDto: UpdateTrackDto): TrackEntity {
-    let updatedTrack = TracksService.tracks.find((user) => user.id === id);
+    let updatedTrack = this.tracks.find((user) => user.id === id);
 
     if (!updatedTrack) throw new NotFoundException();
 
@@ -37,10 +48,15 @@ export class TracksService {
   }
 
   delete(id): TrackEntity {
-    const index = TracksService.tracks.findIndex((user) => user.id === id);
+    const index = this.tracks.findIndex((user) => user.id === id);
+    const favouritesIndex =
+      this.favouritesService.favourites.tracks.indexOf(id);
+
+    if (favouritesIndex !== -1)
+      this.favouritesService.favourites.tracks.splice(favouritesIndex, 1);
 
     if (index === -1) throw new NotFoundException();
 
-    return TracksService.tracks.splice(index, 1)[0];
+    return this.tracks.splice(index, 1)[0];
   }
 }
