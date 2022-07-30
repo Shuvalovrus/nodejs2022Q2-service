98fe6ebe0b4d5c73c2c0ac6d81428990d9301e3d
Konstantin <shuvalov.2021@list.ru>
2022-07-30 15:35:56 +0300

    feat: add migration

--- dataSource.ts
+++ dataSource.ts
@@ -1,20 +1,15 @@
+import { DataSource } from 'typeorm';
 import * as dotenv from 'dotenv';
-import { DataSourceOptions } from 'typeorm';
 
 dotenv.config();
 
-export default {
+export const dataSource = new DataSource({
   type: 'postgres',
-  host: process.env.POSTGRES_DATABASE as string,
+  host: 'localhost',
   port: parseInt(process.env.POSTGRES_PORT as string, 10) as number,
   username: process.env.POSTGRES_USER as string,
   password: process.env.POSTGRES_PASSWORD as string,
   database: process.env.POSTGRES_DATABASE as string,
-  synchronize: false,
-  entities: ['dist/src/**/*.entity.js'],
+  entities: ['dist/src/**/entity/*.entity.js'],
   migrations: ['dist/src/migration/*.{js,ts}'],
-  migrationsRun: false,
-  cli: {
-    migrationsDir: 'src/migrations',
-  },
-} as DataSourceOptions;
+});
